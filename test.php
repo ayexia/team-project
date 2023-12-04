320cb31f221daf924c598ae2c4e4d2ed6ffcf11e
ayexia <79761489+ayexia@users.noreply.github.com>
2023-12-04 06:59:13 +0000

    Add files via upload

---
+++ test.php
@@ -0,0 +1,18 @@
+<head>
+    <title>Michelangelo</title>
+</head>
+<html>
+<body>
+<?php
+
+   echo "<font size='50' face='Georgia' color='ffffff'><center><br><br><br>HELLO WORLD <br><br><font size ='5'> Work in progress";
+   echo "<body style='background-color:tan'>";
+
+?>
+
+    <form method="GET" action="search.php">
+        <input type="text" name="query" placeholder="Type for an item">
+        <button type="submit">Search</button>
+    </form>
+</body>
+</html>
